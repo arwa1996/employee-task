b7af2486849a9f4439826f0edf0add0bec9137c8
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-08 20:14:59 +0200

    feat(App.tsx): create app layout
    create app layout and adding styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, Layout } from 'antd';
+import { HeaderMVP } from './components/Layout/Header/Header';
+import TableEmployee from './components/Table/TableProjects/Table';
+import styles from './App.module.scss';
+import './App.module.scss';
 
 function App() {
+  const { Content } = Layout;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className={styles.layoutBg}>
+      <HeaderMVP />
+      <Content className={styles.contentStyle}>
+        <Card className={styles.cardStyle}>
+          <TableEmployee />
+        </Card>
+      </Content>
+    </Layout>
   );
 }
 
