fb2a6dd5bd38ead537fc8d29f16fb421336e8729
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-10 19:59:52 +0200

    feat(Header): Add header components

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Layout } from 'antd';
+import styles from './Header.module.scss';
+
+export const HeaderMVP: React.FC = () => {
+  const { Header } = Layout;
+
+  return (
+    <Header className={styles.headerStyle}>
+      <a className={styles.headerLogoStyle} href='https://workmotion.com/'>
+        <img
+          src='https://workmotion.com/wp-content/uploads/2021/12/logo-workmotion.svg'
+          alt='workmotion'
+        />
+      </a>
+    </Header>
+  );
+};
