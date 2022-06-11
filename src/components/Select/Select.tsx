e1280b29b14a8ae3f98f0b448c08b2b0d13c7d91
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-11 18:14:48 +0200

    feat: create select component

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Select as SelectEmployeeStatus } from 'antd';
+
+type selectProps = {
+  onChange?: (value: string) => void;
+  placeholder?: string;
+  options?: string[];
+};
+
+export const Select: React.FC<selectProps> = ({
+  onChange,
+  options,
+  placeholder,
+}) => {
+  const { Option } = SelectEmployeeStatus;
+  return (
+    <SelectEmployeeStatus
+      size='large'
+      placeholder={placeholder}
+      onChange={(value: string) => {
+        if (onChange) onChange(value);
+      }}
+    >
+      {options?.map((option, index) => (
+        <Option value={option} key={index}>
+          {option}
+        </Option>
+      ))}
+    </SelectEmployeeStatus>
+  );
+};
