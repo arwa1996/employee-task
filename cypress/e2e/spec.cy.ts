eb8bbee3d0ec7d0584de22f64f0d2ce811dc5bef
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-12 11:13:51 +0200

    chore: install cypress

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+// var employeeName = 'Cypress';
+// var employeeStatus = 'Added';
+
+describe('Input form', () => {
+  it('opens the site', () => {
+    cy.visit('localhost:3001'); //this is the server that was started earlier
+  });
+  it('should open modal when user clicks on Add employee', () => {
+    //get btn
+    cy.get('.ant-btn').click();
+    cy.get('.ant-modal').should('exist');
+  });
+  it('add employee modal should have input field and select field', () => {
+    //get btn
+    cy.get('.ant-input').should('exist');
+    cy.get('.ant-select-selector').should('exist');
+  });
+  it('show alert messages when submitting empty fields', () => {
+    //get btn
+    cy.get('.ant-btn-primary > span').click();
+  });
+});
