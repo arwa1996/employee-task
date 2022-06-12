d34e78e6a367ebc9bfd5c5dc04ce8123dad1d8f6
Arwa mohammed <arwa.elmowafy1996@gmail.com>
2022-06-12 12:05:07 +0200

    feat: add loading component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import styles from './App.module.scss';
 import { HomePage } from './pages/HomePage/HomePage';
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { Loading } from './components/Loader/Loading';
 
 function App() {
   const { Content } = Layout;
@@ -14,6 +15,7 @@ function App() {
       <Layout className={styles.layoutBg}>
         <HeaderEmployee />
         <Content className={styles.contentStyle}>
+          <Loading />
           <HomePage />
         </Content>
       </Layout>
